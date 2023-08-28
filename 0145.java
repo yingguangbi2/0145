b319fa489122e6db41dc0d78531621c6ed3dacf5
yingguangbi2 <142958926+yingguangbi2@users.noreply.github.com>
2023-08-28 15:05:51 +0800

    Create 0145.java

---
+++ 0145.java
@@ -0,0 +1,10 @@
+public class IfStatement {
+  public static void main(String[] args) {
+    int number = 10;
+    if (number > 5) {
+      System.out.println("The number is greater than 5.");
+    } else {
+      System.out.println("The number is less than or equal to 5.");
+    }
+  }
+}
